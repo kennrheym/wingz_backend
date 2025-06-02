d663dbbe6d6478814a63f573796c95523992488e
Kennrhey <mirafloresken1122@gmail.com>
2025-06-02 14:57:46 +0800

    fix: change viewset to modelviewset

--- api/views.py
+++ api/views.py
@@ -4,47 +4,53 @@ from api.serializers import UserSerializer, RideSerializer, RideEventSerializer
 from rest_framework import viewsets
 from rest_framework.response import Response
 
-class UserViewSet(viewsets.ViewSet):
-    """
-    A simple ViewSet for listing or retrieving users.
-    """
-    def list(self, request):
-        queryset = User.objects.all()
-        serializer = UserSerializer(queryset, many=True)
-        return Response(serializer.data)
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    # """
+    # A simple ViewSet for listing or retrieving users.
+    # """
+    # def list(self, request):
+    #     queryset = User.objects.all()
+    #     serializer = UserSerializer(queryset, many=True)
+    #     return Response(serializer.data)
 
-    def retrieve(self, request, pk=None):
-        queryset = User.objects.all()
-        user = get_object_or_404(queryset, pk=pk)
-        serializer = UserSerializer(user)
-        return Response(serializer.data)
+    # def retrieve(self, request, pk=None):
+    #     queryset = User.objects.all()
+    #     user = get_object_or_404(queryset, pk=pk)
+    #     serializer = UserSerializer(user)
+    #     return Response(serializer.data)
     
-class RideViewSet(viewsets.ViewSet):
-    """
-    A simple ViewSet for listing or retrieving rides.
-    """
-    def list(self, request):
-        queryset = Ride.objects.all()
-        serializer = RideSerializer(queryset, many=True)
-        return Response(serializer.data)
+class RideViewSet(viewsets.ModelViewSet):
+    queryset = Ride.objects.all()
+    serializer_class = RideSerializer
+    # """
+    # A simple ViewSet for listing or retrieving rides.
+    # """
+    # def list(self, request):
+    #     queryset = Ride.objects.all()
+    #     serializer = RideSerializer(queryset, many=True)
+    #     return Response(serializer.data)
 
-    def retrieve(self, request, pk=None):
-        queryset = Ride.objects.all()
-        user = get_object_or_404(queryset, pk=pk)
-        serializer = RideSerializer(user)
-        return Response(serializer.data)
+    # def retrieve(self, request, pk=None):
+    #     queryset = Ride.objects.all()
+    #     user = get_object_or_404(queryset, pk=pk)
+    #     serializer = RideSerializer(user)
+    #     return Response(serializer.data)
 
-class RideEventViewSet(viewsets.ViewSet):
-    """
-    A simple ViewSet for listing or retrieving ride events.
-    """
-    def list(self, request):
-        queryset = RideEvent.objects.all()
-        serializer = RideEventSerializer(queryset, many=True)
-        return Response(serializer.data)
+class RideEventViewSet(viewsets.ModelViewSet):
+    queryset = RideEvent.objects.all()
+    serializer_class = RideEventSerializer
+    # """
+    # A simple ViewSet for listing or retrieving ride events.
+    # """
+    # def list(self, request):
+    #     queryset = RideEvent.objects.all()
+    #     serializer = RideEventSerializer(queryset, many=True)
+    #     return Response(serializer.data)
 
-    def retrieve(self, request, pk=None):
-        queryset = RideEvent.objects.all()
-        user = get_object_or_404(queryset, pk=pk)
-        serializer = RideEventSerializer(user)
-        return Response(serializer.data)
+    # def retrieve(self, request, pk=None):
+    #     queryset = RideEvent.objects.all()
+    #     user = get_object_or_404(queryset, pk=pk)
+    #     serializer = RideEventSerializer(user)
+    #     return Response(serializer.data)
