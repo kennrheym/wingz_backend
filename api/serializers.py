8b240d4c5c1455333aba24a5c412f85d653347f4
Kennrhey <mirafloresken1122@gmail.com>
2025-06-04 17:15:41 +0800

    feat: add distance to ride list api data (distance sort wip)

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import User, Ride, RideEvent
-
+from geopy.distance import geodesic
+from .enums import RideStatus
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -16,16 +17,18 @@ class UserSerializer(serializers.ModelSerializer):
         user.save()
         return user
     
-class RideEventSerializer(serializers.ModelSerializer):
-    # id_ride = RideSerializer(read_only=True)
 
+class RideEventSerializer(serializers.ModelSerializer):
     class Meta:
         model = RideEvent
         fields = ['id', 'id_ride', 'description', 'created_at']
 
+
 class RideSerializer(serializers.ModelSerializer):
     id_rider = UserSerializer(read_only=True)
     id_driver = UserSerializer(read_only=True)
+    distance = serializers.SerializerMethodField()
+
     ride_events = RideEventSerializer(source='rideevent_set', many=True, read_only=True)
 
     class Meta:
@@ -40,8 +43,21 @@ class RideSerializer(serializers.ModelSerializer):
             'dropoff_latitude',
             'dropoff_longitude',
             'pickup_time',
-            'ride_events'
+            'ride_events',
+            'distance'
         ]
+    
+    def get_distance(self, obj):
+        driver_lat = self.context.get('driver_latitude')
+        driver_long = self.context.get('driver_longitude')
+        if driver_lat and driver_long and obj.status == RideStatus.EN_ROUTE:
+            try:
+                coord1 = (float(driver_lat), float(driver_long))
+                coord2 = (obj.pickup_latitude, obj.pickup_longitude)
+                return round(geodesic(coord1, coord2).kilometers, 2)
+            except Exception:
+                return None
+        return None
 
 
 
--- api/views.py
+++ api/views.py
@@ -7,6 +7,9 @@ from api.permissions import IsAdminRole
 from api.pagination import DefaultPagination
 from django_filters.rest_framework import DjangoFilterBackend
 from api.filters import RideFilter
+from .enums import RideStatus
+from geopy.distance import geodesic
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
@@ -22,10 +25,69 @@ class RideViewSet(viewsets.ModelViewSet):
     serializer_class = RideSerializer
     permission_classes=[IsAdminRole]
     pagination_class = DefaultPagination
-    filter_backends = [filters.OrderingFilter, DjangoFilterBackend]
-    ordering_fields = ['pickup_time']  # Customize based on your model
-    ordering = ['-pickup_time']  # Default ordering
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+
+    # filter_backends = [DjangoFilterBackend]
+    ordering_fields = ['pickup_time']
+    ordering = ['-pickup_time']
     filterset_class = RideFilter
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context['driver_latitude'] = self.request.query_params.get('driver_latitude')
+        context['driver_longitude'] = self.request.query_params.get('driver_longitude')
+        return context
+    
+    # def get_queryset(self):
+    #     queryset = super().get_queryset()
+
+    #     driver_latitude = self.request.query_params.get('driver_latitude')
+    #     driver_longitude = self.request.query_params.get('driver_longitude')
+    #     order_by_distance = self.request.query_params.get('ordering') == 'distance'
+
+    #     if order_by_distance and driver_latitude and driver_longitude:
+    #         driver_coord = (float(driver_latitude), float(driver_longitude))
+
+    #         # Convert to list to annotate and sort
+    #         queryset = list(queryset)
+    #         for ride in queryset:
+    #             ride.distance = geodesic(
+    #                 driver_coord,
+    #                 (ride.pickup_latitude, ride.pickup_longitude)
+    #             ).kilometers
+
+    #         queryset.sort(key=lambda r: r.distance)
+
+    #     return queryset
+    
+    # def paginate_queryset(self, queryset):
+    #     if isinstance(queryset, list):
+    #         paginator = self.pagination_class()
+    #         page_size = paginator.get_page_size(self.request)
+    #         if not page_size:
+    #             return None
+
+    #         page_number = self.request.query_params.get(paginator.page_query_param, 1)
+    #         try:
+    #             page_number = int(page_number)
+    #         except ValueError:
+    #             page_number = 1
+
+    #         start = (page_number - 1) * page_size
+    #         end = start + page_size
+    #         paginated_list = queryset[start:end]
+
+    #         # Store pagination info for response
+    #         self._paginator = paginator
+    #         self._paginator.page = page_number
+    #         self._paginator.count = len(queryset)
+    #         self._paginator.num_pages = (len(queryset) + page_size - 1) // page_size
+
+    #         return paginated_list
+    #     else:
+    #         return super().paginate_queryset(queryset)
+
+
 
 class RideEventViewSet(viewsets.ModelViewSet):
     queryset = RideEvent.objects.all()
