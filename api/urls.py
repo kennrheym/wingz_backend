16d8ee97d445288790c3549529c5152f3aff91b9
Kennrhey <mirafloresken1122@gmail.com>
2025-06-06 05:28:05 +0800

    fix: RideEventViewSet instead of RideViewSet

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
 # from .views import BookListCreateAPIView, BookDetailAPIView
 from rest_framework.routers import DefaultRouter
-from api.views import UserViewSet, RideViewSet, RideViewSet
+from api.views import UserViewSet, RideViewSet, RideEventViewSet
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
 router.register(r'rides', RideViewSet, basename='ride')
-router.register(r'ride_events', RideViewSet, basename='ride_event')
+router.register(r'ride_events', RideEventViewSet, basename='ride_event')
 
 urlpatterns = [
     path('', include(router.urls)),
